f1b6df1c802dca8874ce7e68fa168c8df6ae43e9
Dikesh Sapkota <dikeshsapkota@gmail.com>
2026-06-23 12:45:23 +0545

    update in styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,11 @@
-import viteLogo from "../assets/vite.svg";
+import viteLogo from "../assets/react-logo.png";
 function Header() {
     return (
         <header className="header">
             <img src={viteLogo} className="nav-logo" alt="Vite logo" />
+            
             <nav>
-   
+         
       <ul className="nav-links">
         <li>Home</li>
         <li>About</li>
